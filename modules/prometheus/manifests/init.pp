c564833e4ac58ee9ed6798b042ce1482ff23b09f
Shaymedina <73135109+Shaymedina@users.noreply.github.com>
2020-10-26 20:01:46 +0200

    Update init.pp

--- modules/prometheus/manifests/init.pp
+++ modules/prometheus/manifests/init.pp
@@ -1,1 +1,16 @@
+class prometheus::install {
+	package { 'prometheus': }
+	file { '/etc/prometheus/prometheus.yml':
+		require => Package['prometheus'],
+		source => 'puppet:///modules/prometheus/prometheus.yml',
+		owner => 'root',
+		group => 'root',
+	}
 
+  	service { 'prometheus':
+    	ensure    => true,
+    	enable    => true,
+    	hasstatus => true,
+    	require   => Package['prometheus'],
+  	}
+}
