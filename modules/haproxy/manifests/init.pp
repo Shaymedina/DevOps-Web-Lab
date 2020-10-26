ab176c4961a3f65a262315353a06580e1cf5ff35
Shaymedina <73135109+Shaymedina@users.noreply.github.com>
2020-10-26 19:58:15 +0200

    Update init.pp

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -1,1 +1,24 @@
+class haproxy::install {
+	package { 'haproxy': }
+	file { '/etc/haproxy/haproxy.cfg':
+		require => Package['haproxy'],
+		source => 'puppet:///modules/haproxy/haproxy.cfg',
+		owner => 'root',
+		group => 'root',
+	}
 
+	file { '/etc/default/haproxy':
+		require => Package['haproxy'],
+		source => 'puppet:///modules/haproxy',
+		owner => 'root',
+		group => 'root',
+	}
+
+  	service { 'haproxy':
+    	ensure    => true,
+    	enable    => true,
+    	hasstatus => true,
+    	restart   => '/usr/sbin/service haproxy reload',
+    	require   => Package['haproxy'],
+  	}
+}
