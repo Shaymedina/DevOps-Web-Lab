ef67736c15bf989cc29e26e8c3a6d6485d3659e1
Shaymedina <73135109+Shaymedina@users.noreply.github.com>
2020-10-26 19:57:17 +0200

    Update init.pp

--- modules/alertmanager/manifests/init.pp
+++ modules/alertmanager/manifests/init.pp
@@ -1,1 +1,21 @@
+class alertmanager::install (
+  $package_name = 'prometheus-alertmanager',
+  $service_name = 'prometheus-alertmanager',
+  $doc_root = '/etc/prometheus'
+) {
+  package { $package_name:
+    ensure => present,
+  }
+  service { $service_name:
+    ensure    => true,
+    enable    => true,
+
+  }
+  file { "$doc_root/alertmanager.yml":
+    source => "puppet:///modules/alertmanager/alertmanager.yml",
+  }
+  file { "$doc_root/alert.rules.yml":
+    source => "puppet:///modules/alertmanager/alert.rules.yml",
+  }
+}
 
