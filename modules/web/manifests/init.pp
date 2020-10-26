b2745467e28845933606eca0e24986e0a9787b90
Shaymedina <73135109+Shaymedina@users.noreply.github.com>
2020-10-26 20:03:03 +0200

    Update init.pp

--- modules/web/manifests/init.pp
+++ modules/web/manifests/init.pp
@@ -1,1 +1,19 @@
+class web::nginx (
+  $package_name = 'nginx',
+  $service_name = 'nginx',
+  $doc_root = '/var/www/html'
+) {
+  package { $package_name:
+    ensure => present,
+  }
+  service { $service_name:
+    ensure => running,
+    enable => true,
+  }
+  file { "$doc_root/index.html":
+    source => "puppet:///modules/hello_web/index.html",
+  }
+}
+
+
 
