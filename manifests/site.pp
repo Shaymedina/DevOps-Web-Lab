0c1a0f202dcf967af020ad8e269af336920be8fb
Shaymedina <73135109+Shaymedina@users.noreply.github.com>
2020-10-26 19:55:28 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,15 @@
 
+node 'web1', 'web2' {
+   #class { 'web': }
+   include 'node_exporter::install'
+   include 'web::nginx'   
+}
+   
+node 'loadbalancer' {
+   #class { 'haproxy': }
+   include 'haproxy::install'  
+   include 'prometheus::install' 
+   include 'alertmanager::install'    
+  }
+  
+  
