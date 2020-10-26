ee1bbc00ec0e1a430582f6c37c17db0b4e38cd46
Shaymedina <73135109+Shaymedina@users.noreply.github.com>
2020-10-26 20:00:28 +0200

    Create init.pp

---
+++ modules/node_exporter/manifests/init.pp
@@ -0,0 +1,13 @@
+ class node_exporter::install (
+  $package_name = 'prometheus-node-exporter',
+  $service_name = 'prometheus-node-exporter',
+  )       {
+  package { $package_name:
+    ensure => present,
+  }
+  service { $service_name:
+    ensure => running,
+    enable => true,
+  }
+}
+
